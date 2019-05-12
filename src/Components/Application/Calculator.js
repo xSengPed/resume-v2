73d7c3175777f777a0779d733bc7a80de3beb548
DONNUKRIT SATIRAKUL <kalima007c@gmail.com>
2019-05-13 00:43:48 +0700

    function complete

--- src/Components/Application/Calculator.js
+++ src/Components/Application/Calculator.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Container} from 'reactstrap'
 import GradeSection from './Components/GradeSection'
+import {Tabs} from 'antd'
 import './css/appCal.css'
 
+
+function callback(key) {
+    console.log(key);
+  }
+  
 class Calculator extends React.Component {
     render(){
+        const TabPane = Tabs.TabPane;
         return(
-            <Container className = "box-container">
-                <div>Grade Cal</div>
-                <div className="centered"><GradeSection /></div>
+            <Container className = "card-container">
+                <GradeSection/>
             </Container>
         )
     }
--- src/Components/Application/Components/GradeSection.js
+++ src/Components/Application/Components/GradeSection.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import {Select,Col,Row,InputNumber} from 'antd'
+import {Select,Col,Row,InputNumber, Alert ,Divider} from 'antd'
 import {Container} from 'reactstrap'
 import '../css/appCal.css'
 
-
-const inputWidth = {
-    width : "150px"
-}
-const Padding = {
-    paddingLeft : "10px"
-}
-
-const Option = Select.Option
 class GradeSection extends React.Component {
-
 
     constructor(props){
         super(props)
@@ -25,25 +15,48 @@ class GradeSection extends React.Component {
           b:'',
         };
       }
-     
+    
     loadData = async() => {
         const {a,b,c,gpa,gpac} = this.state
-        
-        
     }
 
-
-    onChangeHandlerA = (value) => {
-        this.setState({a : value},() => {
-            this.loadData(value)
-        })
-        
+    calgpa = () =>{
+      const temp =  this.state.gpac/this.state.c;
+      this.setState({gpa:temp.toFixed(2)})
     }
-    onChangeHandlerB= (value) => {
-     this.setState({b : value},() => {
-         this.loadData(value)
-     })
-     
+    handleChangec = (value) => {
+      this.setState({ c: value}, () => {
+        this.loadData(value)
+      })
+    }
+    handleChangegpac = (value) => {
+      this.setState({ gpac: value}, () => {
+        this.loadData(value)
+      })
+      
+    }
+    handleChangegpa = (value) => {
+      this.setState({ gpa: value });
+    }
+    one = (value) => {
+      this.setState({ a: value},()=>{
+        this.loadData(value)
+      })
+    }
+    two = (value) => {
+      this.setState({ b: value},()=>{
+        this.loadData(value)
+        this.three()
+      })
+    }
+    three = () => {
+      const temp =  this.state.b*this.state.a;
+      var credit = +this.state.a + +this.state.c
+      var point= +temp + +this.state.gpac
+      this.setState({ 
+        gpac: point,
+        c :credit
+      }, () => this.calgpa())
     }
     render(){
         const Option = Select.Option;
@@ -52,21 +65,18 @@ class GradeSection extends React.Component {
         for(let i = 0 ; i < 10 ; i++)
         {
             rows.push (
-                 <Row style={{padding:"10px"}}>
-                    <Col span={4}>Subject</Col>
-                    <Col span={4}>{'0'+(i+1)}</Col>
-                    <Col span={4}>Unit</Col>
-                    <Col span={4}>
-                        <Select style={{width:"75px"}} onChange = {this.onChangeHandlerA.bind(this)} defaultValue = "0">
+                 <Row style={{paddingBottom:"10px",paddingTop:"10px",textAlign:"center"}}>
+                    <Col span={8}>{'0'+(i+1)}</Col>
+                    <Col span={8}>
+                        <Select style={{width:"75px"}} onChange = {this.one.bind(this)} defaultValue = "0">
                             <Option value = "0">0</Option>
                             <Option value = "1">1</Option>
                             <Option value = "2">2</Option>
                             <Option value = "3">3</Option>
                             <Option value = "4">4</Option>
                         </Select></Col>
-                    <Col span={4}>Grade</Col>
-                    <Col span={4}>
-                        <Select style={{width:"75px"}} onChange = {this.onChangeHandlerB} defaultValue = "4">
+                    <Col span={8}>
+                        <Select style={{width:"75px"}} onChange = {this.two.bind(this)} defaultValue = "-">
                             <Option value = "4">A</Option>
                             <Option value = "3.5">B+</Option>
                             <Option value = "3">B</Option>
@@ -79,20 +89,24 @@ class GradeSection extends React.Component {
             </Row>
             )
         }
-        
-       
-    
         return (
-          <Container className="box-container">
-                {rows}             
-                <div> Curr UNIT Select : 
-                {this.state.a}
-                {console.log("UNIT UPDATE : "+this.state.a)}
-                </div>
+          <Container>
+            <Divider><div style = {{fontSize:"3vw",paddingTop:"20px"}} className="app-header"><i>Grade Prediction System</i></div></Divider>
+          <Row style={{textAlign:"center",paddingTop:"20px"}}>
+            <Col span={8}>No.</Col>
+            <Col span={8}>Credit</Col>
+            <Col span={8}>Grade</Col>
+          </Row>
+                {rows}
                 <div>
-                    Curr GRADE Select : 
-                {this.state.b}
-                {console.log("GRADE UPDATE : "+this.state.b)}
+                  <div>CUMULATIVE CREDITS : {}<InputNumber min = {0} onChange={this.handleChangec} defaultValue={37}/></div>
+                  <div>CUMULATIVE GRADE POINT AVERAGE CREDITS : {}<InputNumber min = {0} onChange={this.handleChangegpac} defaultValue={112}/></div>
+                </div>             
+                <div style={{textAlign:"center" ,paddingTop:"10px",paddingBottom:"10px"}}>
+                    <Alert type="success" message={'CUMULATIVE GRADE POINT AVERAGE : '+ +this.state.gpa}/>
+                </div>
+                <div style={{textAlign:"center"}}>
+                  <Alert type="info" message = "Special Thank : airx.me (Back-End)"/>
                 </div>
           </Container>
         )
